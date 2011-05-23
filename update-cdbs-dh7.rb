54581cfdc6515c7c8e55316aeb4960513078aca8
Lucas Nussbaum <lucas@lucas-nussbaum.net>
2011-05-23 09:17:57 +0200

    update stats

--- update-cdbs-dh7.rb
+++ update-cdbs-dh7.rb
@@ -2,6 +2,8 @@
 
 require 'date'
 
+BROKEN_DATES = ['2011-05-16', '2011-05-17', '2011-05-18', '2011-05-19', '2011-05-20']
+
 system("scp lintian.debian.org:~cjwatson/dhstats.txt .")
 f = File::new("cdbs-dh7.txt", 'w')
 f.puts "date dh dh7 cdbs"
@@ -13,6 +15,7 @@ IO::read("dhstats.txt").split(/\n\n/).each do |par|
   dh = lines.grep(/^debhelper: /)[0].split[2].gsub(/[\(\)\%]/,'').to_f
   dh7 = lines.grep(/^dh\(1\): /)[0].split[2].gsub(/[\(\)\%]/,'').to_f
   cdbs = lines.grep(/^CDBS: /)[0].split[2].gsub(/[\(\)\%]/,'').to_f
+  next if BROKEN_DATES.include?(date)
   f.puts "#{date} #{dh - dh7 - cdbs} #{dh7} #{cdbs}"
 end
 f.close
